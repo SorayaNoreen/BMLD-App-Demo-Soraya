45327dfbd45d2e07cc6aced3fa54ad4dbff8faed
SorayaNoreen <gfrersor@students.zhaw.ch>
2025-02-26 20:31:37 +0100

    emoji versuch

--- Start.py
+++ Start.py
@@ -4,4 +4,6 @@ import pandas as pd
 st.title("Meine erste Streamlit App")
 
 st.write("Diese App wurde von Soraya Gfrerer entwickelt.")
-st.write("E-Mail Adresse: gfrersor@students.zhaw.ch.")       
+st.write("E-Mail Adresse: gfrersor@students.zhaw.ch.")  
+
+st.markdown("<h1 style='font-size:100px;'>&#128521;</h1>", unsafe_allow_html=True)
