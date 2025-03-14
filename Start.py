6124f1ea401f6d723f335f4a08d4addcc4d35772
SorayaNoreen <gfrersor@students.zhaw.ch>
2025-03-14 08:59:29 +0100

    Update Start.py

--- Start.py
+++ Start.py
@@ -1,9 +1,13 @@
-import streamlit as st
 import pandas as pd
 from utils.data_manager import DataManager
+from utils.login_manager import LoginManager
 
 # initialize the data manager
-data_manager = DataManager(fs_protocol='webdav', fs_root_folder="BMLD_App_DB")  # switch drive 
+data_manager = DataManager(fs_protocol='webdav', fs_root_folder="LN1_Demo_App")  # switch drive 
+
+# initialize the login manager
+login_manager = LoginManager(data_manager)
+login_manager.login_register()  # open login/register page
 
 # load the data from the persistent storage into the session state
 data_manager.load_user_data(
@@ -12,6 +16,8 @@ data_manager.load_user_data(
     initial_value = pd.DataFrame(), 
     parse_dates = ['timestamp']
     )
+
+import streamlit as st
 
 st.title("Kalorienrechner")
 
